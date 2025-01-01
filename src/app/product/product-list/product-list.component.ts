caeff5982c5dd87f0043b34245a218ea3109d9bc
saeed.da1994 <saeed.da1994@gmail.com>
2025-01-01 23:00:22 +0330

    get product from mock service

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../product.service";
+import {Product} from "../../models/product";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe((data: Product[]) => {
+      this.products = data;
+    })
+  }
 }
