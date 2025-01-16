a10d1a6c255587cc0abec2fc0af606f79e354203
saeed.da1994 <saeed.da1994@gmail.com>
2025-01-16 20:53:52 +0330

    generate card service module and component

---
+++ src/app/card/card.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardViewComponent } from './card-view/card-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CardModule { }
